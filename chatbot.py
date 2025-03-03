0252c55e5df122212726cd4a401648289e7e0f02
adson-fl <adsonferreira585@gmail.com>
2025-03-03 09:28:40 -0300

    Conclusão_do_Projeto

--- chatbot.py
+++ chatbot.py
@@ -12,10 +12,35 @@ genai.configure(api_key=api_key)
 
 model = genai.GenerativeModel("gemini-1.5-flash-latest")
 
-response = model.generate_content("quem criou os modelos de IA GEMINI ?")
+def apresentacao():
+  print("Bem-Vindo ao projeto Chatbot")
+  print("Digite 'sair' a qualquer momento para encerra o Chat")
 
- # Verificando a resposta
-if response:
-  print(response.text)
-else:
-  print("Erro: Não houve resposta do modelo.")
+def main():
+
+  apresentacao()
+
+  # Lista de Historico de Chats
+  chat = model.start_chat(history=[])
+
+  while True:
+
+    # Pergunta digitada
+    user_pergunta = input("\nVocê:")
+
+    # Condição de finalização 
+    if user_pergunta.lower() == "sair":
+      break
+
+    # Formatação de Saida
+    gemini_bot = f"\nUsuário: {user_pergunta}\nChatbot:"
+
+    # Enviando pergunta e guardando na lista
+    response = chat.send_message(gemini_bot)
+
+    print(f"\nChatbot:{response.text}")
+
+if __name__ == "__main__":
+  main()
+
+    
